adeca3178a4ef68442be876eb28d97251c395dcc
Emily Parham <eparham207@gmail.com>
2024-01-27 22:53:05 -0600

    toast commit

--- ITC 362/Week 2/GeoQuiz/app/src/main/java/com/parham/msu/geoquiz/MainActivity.kt
+++ ITC 362/Week 2/GeoQuiz/app/src/main/java/com/parham/msu/geoquiz/MainActivity.kt
@@ -2,10 +2,37 @@ package com.parham.msu.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton : Button
+    private lateinit var falseButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.False_button)
+
+        trueButton.setOnClickListener{view: View ->
+            Toast.makeText(
+                this,
+                R.string.true_button,
+                Toast.LENGTH_SHORT)
+                .show()
+
+        }
+        falseButton.setOnClickListener{view: View ->
+            Toast.makeText(
+                this,
+                R.string.false_button,
+                Toast.LENGTH_SHORT)
+                .show()
+
+        }
     }
 }
